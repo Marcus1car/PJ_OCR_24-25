49969a5b9fb9f6e2efad5c16d469703c37278b2b
Marcus1car <marcuspoirot04@gmail.com>
2024-10-31 17:20:15 +0100

    put images in assets file , fixed indentation

--- src/preprocessing/man_rota.c
+++ src/preprocessing/man_rota.c
@@ -1,7 +1,7 @@
-#include <SDL2/SDL.h>        // SDL core library for rendering and surfaces
-#include <SDL2/SDL_image.h>   // SDL image library, if needed for loading non-BMP images
+#include <SDL2/SDL.h>        
+#include <SDL2/SDL_image.h>   
 #include <math.h>    
-#define M_PI 3.14159265358979323846         
+#define M_PI 3.14159265358979323846    //Just in case      
 
 SDL_Surface* loadImage(const char* given_path) 
 {
@@ -20,7 +20,7 @@ SDL_Surface* loadImage(const char* given_path)
     }
     return image;
 }
-SDL_Surface* rotate_image(SDL_Surface *image, double angle) {
+SDL_Surface* manualrota(SDL_Surface *image, double angle) {
     double radians = angle * 3.141593 / 180.0;
 
     int width = image->w;
@@ -30,11 +30,11 @@ SDL_Surface* rotate_image(SDL_Surface *image, double angle) {
     int new_width = (int)(fabs(width * cos(radians)) + fabs(height * sin(radians)));
     int new_height = (int)(fabs(width * sin(radians)) + fabs(height * cos(radians)));
 
-    SDL_Surface *rotated_image = SDL_CreateRGBSurface(
+    SDL_Surface *res = SDL_CreateRGBSurface(
         0, new_width, new_height, 32, 0x00FF0000, 0x0000FF00, 0x000000FF, 0xFF000000
     );
 
-    if (!rotated_image) {
+    if (!res) {
         printf("Failed to create rotated surface: %s\n", SDL_GetError());
         return NULL;
     }
@@ -46,7 +46,7 @@ SDL_Surface* rotate_image(SDL_Surface *image, double angle) {
     int middle_y_new = new_height / 2;
 
     Uint32 *original_pixels = (Uint32 *)image->pixels;
-    Uint32 *rotated_pixels = (Uint32 *)rotated_image->pixels;
+    Uint32 *rotated_pixels = (Uint32 *)res->pixels;
 
     // Iterate through all pixels of the new image
     for (int y_new = 0; y_new < new_height; y_new++) {
@@ -60,44 +60,41 @@ SDL_Surface* rotate_image(SDL_Surface *image, double angle) {
                 rotated_pixels[y_new * new_width + x_new] = original_pixels[y_old * width + x_old];
             } else {
                 // Set to transparent for out-of-bounds pixels
-                rotated_pixels[y_new * new_width + x_new] = SDL_MapRGBA(rotated_image->format, 0, 0, 0, 0);
+                rotated_pixels[y_new * new_width + x_new] = SDL_MapRGBA(res->format, 0, 0, 0, 0);
             }
         }
     }
 
-    return rotated_image;
+    return res;
 }
-
 
 int main(int argc, char *argv[]) 
 {
-    if (argc != 3) {
+    if (argc != 3) 
+    {
         printf("Usage: %s <input_image.bmp> <angle>\n", argv[0]);
         return 1;
     }
-
-    const char *input_file = argv[1];
+    const char *input = argv[1];
     double angle = atof(argv[2]);
 
     // Load the image
-    SDL_Surface *image = loadImage(input_file); // Ensure this function is defined or included
-    if (!image) {
+    SDL_Surface *image = loadImage(input); // Ensure this function is defined or included
+    if (!image) 
+    {
         printf("Failed to load image: %s\n", IMG_GetError());
         return 1;
     }
-
     // Rotate the image
-    SDL_Surface *rotated_image = rotate_image(image, angle);
-    if (!rotated_image) {
+    SDL_Surface *res = manualrota(image, angle);
+    if (!res) 
+    {
         SDL_FreeSurface(image);
         return 1;
     }
-
-    // Save the rotated image or display it
-    SDL_SaveBMP(rotated_image, "rotated_image.bmp");
-
+    SDL_SaveBMP(res, "image.bmp");// Defines name of the exit file 
     // Cleanup
     SDL_FreeSurface(image);
-    SDL_FreeSurface(rotated_image);
+    SDL_FreeSurface(res);
     return 0;
 }
